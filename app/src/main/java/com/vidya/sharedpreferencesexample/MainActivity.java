31da11b1629e390b7d9ad23f337ef04d38759ea1
shaheennamboori <mailtoshaheen.n.s@gmail.com>
2020-07-08 13:56:05 +0530

    update

--- app/src/main/java/com/vidya/sharedpreferencesexample/MainActivity.java
+++ app/src/main/java/com/vidya/sharedpreferencesexample/MainActivity.java
@@ -2,13 +2,62 @@ package com.vidya.sharedpreferencesexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayText;
+    private EditText enteredText;
+    private Button saveButton,applyButton;
+
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+
+    private String text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        displayText = findViewById(R.id.displayBox);
+        enteredText = findViewById(R.id.enteredText);
+        saveButton = findViewById(R.id.saveButton);
+        applyButton = findViewById(R.id.applyButton);
+
+
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayText.setText(enteredText.getText().toString());
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+    }
+
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT , displayText.getText().toString());
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+
     }
 }
